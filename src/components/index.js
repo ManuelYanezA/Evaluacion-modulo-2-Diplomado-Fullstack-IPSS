b732b3587e4944ae9bb36115186d190ff50238f2
Manuel Yáñez <manuelyanez.ar@gmail.com>
2026-06-29 03:43:12 -0400

    feat: agregar estilos CSS para ErrorMessage y componentes nuevos al index

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,5 @@ export {default as Header} from "./Header/Header";
 export {default as Footer} from "./Footer/Footer";
 export {default as Searchbar} from "./Searchbar/Searchbar";
 export {default as CartDrawer} from "./CartDrawer/CartDrawer";
+export {default as Loader} from "./Loader/Loader";
+export {default as ErrorMessage} from "./ErrorMessage/ErrorMessage";
